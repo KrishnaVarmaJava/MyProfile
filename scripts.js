def6d889dce4c71bb051886fa57f686450192966
Krishna Verma <kvarma@kvarma-ltm.internal.salesforce.com>
2024-08-31 23:12:31 +1000

    add image to profile

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide').length;
+
+function moveRight() {
+    currentSlide = (currentSlide + 1) % slides;
+    updateSlidePosition();
+}
+
+function moveLeft() {
+    currentSlide = (currentSlide - 1 + slides) % slides;
+    updateSlidePosition();
+}
+
+function updateSlidePosition() {
+    document.getElementById('slider').style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+// Auto slide every 10 seconds
+setInterval(moveRight, 10000);
